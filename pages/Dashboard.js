7c9cb6ae95e7f4bb8b48993534927a8df66e120a
mkulupe <Support@Gmobility.local>
2022-12-06 16:20:10 +0200

    Dashboard File Added

---
+++ pages/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Dashboard= () =>(
+    <div>
+        <div>
+            {/* Navigatioon */}
+            <div></div>
+            {/* Content */}
+            <div></div>
+        </div>
+    </div>
+)
+
+export default Dashboard;
