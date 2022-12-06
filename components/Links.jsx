9d31d31d9f80d29cb2d0531619947f9aa6fd86aa
mkulupe <Support@Gmobility.local>
2022-12-06 16:18:36 +0200

    navigation in progress

--- components/Links.jsx
+++ components/Links.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 
 const Links= (props) =>(
-    <a className="py-2.5 text-base text-white" href={props.reference}>{props.links}</a>
+    <a className="py-2.5 text-base text-white px-12 font-semibold font-serif bg-yellow-400" href={props.reference}></a>
 )
 
 export default Links;
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import Links from "./Links";
+import Profilepic from "../public/images/profile.png";
+import Image from "next/image";
 
-
-const Navbar= () =>(
-    <div>
+function Navigation (){
+    return (
         <div>
-            {/* Navigatioon */}
-            <div></div>
-            {/* Content */}
-            <div></div>
-        </div>
-    </div>
-)
+            <div className="shadow-xl py-2.5 bg-yellow-800 grid grid-cols-12">
+                {/* Navigation */}
+                <div className="p-6 col-span-10">
+                    <nav className="">
+                        <Links href={"/"}>Home</Links>
+                    </nav>
+                </div>
+                <Image src={Profilepic} className="w-32 h-32 rounded-full col-span-2"/>
 
-export default Navbar;
+            </div>
+        </div>
+    )
+}
+
+export default Navigation;
